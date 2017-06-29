1d1b01dfaef1da87763a8a123c7b4e1dd11905e5
Theo <b.theo95@gmail.com>
2017-06-29 20:08:41 +0300

    Bug Fix

--- app/src/main/java/com/gmail/btheo95/aria/service/MediaJobService.java
+++ app/src/main/java/com/gmail/btheo95/aria/service/MediaJobService.java
@@ -6,6 +6,8 @@ import android.app.job.JobScheduler;
 import android.app.job.JobService;
 import android.content.ComponentName;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.gmail.btheo95.aria.R;
@@ -46,7 +48,7 @@ public class MediaJobService extends JobService {
     public static void startNewMedia(Context context) {
         ComponentName service = new ComponentName(context, MediaJobService.class);
         JobInfo jobInfo = new JobInfo.Builder(JOB_NEW_MEDIA, service)
-                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+//                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
                 .setPeriodic(3600 * 1000) // 10 minutes - 600000
                 .setBackoffCriteria(60 * 1000, JobInfo.BACKOFF_POLICY_LINEAR)
                 .setRequiresDeviceIdle(false)
@@ -106,6 +108,9 @@ public class MediaJobService extends JobService {
             if (!arePermissionGranted(context)) {
                 return;
             }
+            if (!isConnectedToWifi(context)) {
+                return;
+            }
             Media.updateData(context);
             mMediaUploader = new MediaUploader(context);
             mMediaUploader.startUploading();
@@ -139,6 +144,22 @@ public class MediaJobService extends JobService {
                 return;
             }
             mMediaUploader.stop();
+        }
+    }
+
+    private boolean isConnectedToWifi(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+
+        if (info != null &&
+                info.isConnected() &&
+                info.getType() == ConnectivityManager.TYPE_WIFI) {
+
+            return true;
+
+        } else {
+            return false;
         }
     }
 }
